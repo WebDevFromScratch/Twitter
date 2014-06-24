9311078defb3d64003e49ec91638387f085f97d0
Piotr Klosinski <mybettingstuff@gmail.com>
2014-06-24 21:41:17 +0200

    added more front-end styling, validations for status and error alerts for status creation (in new.html.erb)

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,4 +2,5 @@ class Status < ActiveRecord::Base
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
 
   validates :creator, presence: :true
+  validates :body, presence: :true, length: { minimum: 5 }
 end
